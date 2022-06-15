b93db847484c0c5428d3d1464b561c44ad31e2e3
Brian Xu <brian.xu@torchbox.com>
2022-06-15 15:37:26 +0800

    add migration for wagtail3.0

--- wagtail_guide/migrations/0001_initial.py
+++ wagtail_guide/migrations/0001_initial.py
@@ -20,20 +20,140 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('wagtailcore', '0041_group_collection_permissions_verbose_name_plural'),
+        ("wagtailcore", "0041_group_collection_permissions_verbose_name_plural"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='EditorGuide',
+            name="EditorGuide",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('information_text', models.TextField(blank=True, help_text='Add a leading information paragraph explaining the guide')),
-                ('sections', wagtail_fields.StreamField([('heading', wagtail_blocks.CharBlock(classname='full title', icon='title', template='wagtail_guide/streamfield/blocks/heading_block.html')), ('paragraph', wagtail_blocks.RichTextBlock(features=['h2', 'h3', 'h4', 'h5', 'h6', 'bold', 'italic', 'link', 'ul', 'ol', 'hr'])), ('image', wagtail_blocks.StructBlock([('image', wagtail.images.blocks.ImageChooserBlock()), ('caption', wagtail_blocks.CharBlock(required=False))])), ('quote', wagtail_blocks.StructBlock([('quote', wagtail_blocks.CharBlock(classname='title')), ('attribution', wagtail_blocks.CharBlock(required=False))])), ('pull_quote', wagtail_blocks.StructBlock([('pull_quote', wagtail_blocks.CharBlock(classname='title'))])), ('embed', wagtail_blocks.StructBlock([('title', wagtail_blocks.CharBlock(required=False)), ('embed', wagtail.embeds.blocks.EmbedBlock())])), ('video', wagtail_blocks.StructBlock([('video', wagtail.embeds.blocks.EmbedBlock(label='Video URL', required=False))]))], blank=True)),
-                ('site', models.OneToOneField(editable=False, on_delete=django.db.models.deletion.CASCADE, to='wagtailcore.Site')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "information_text",
+                    models.TextField(
+                        blank=True,
+                        help_text="Add a leading information paragraph explaining the guide",
+                    ),
+                ),
+                (
+                    "sections",
+                    wagtail_fields.StreamField(
+                        [
+                            (
+                                "heading",
+                                wagtail_blocks.CharBlock(
+                                    classname="full title",
+                                    icon="title",
+                                    template="wagtail_guide/streamfield/blocks/heading_block.html",
+                                ),
+                            ),
+                            (
+                                "paragraph",
+                                wagtail_blocks.RichTextBlock(
+                                    features=[
+                                        "h2",
+                                        "h3",
+                                        "h4",
+                                        "h5",
+                                        "h6",
+                                        "bold",
+                                        "italic",
+                                        "link",
+                                        "ul",
+                                        "ol",
+                                        "hr",
+                                    ]
+                                ),
+                            ),
+                            (
+                                "image",
+                                wagtail_blocks.StructBlock(
+                                    [
+                                        (
+                                            "image",
+                                            wagtail.images.blocks.ImageChooserBlock(),
+                                        ),
+                                        (
+                                            "caption",
+                                            wagtail_blocks.CharBlock(required=False),
+                                        ),
+                                    ]
+                                ),
+                            ),
+                            (
+                                "quote",
+                                wagtail_blocks.StructBlock(
+                                    [
+                                        (
+                                            "quote",
+                                            wagtail_blocks.CharBlock(classname="title"),
+                                        ),
+                                        (
+                                            "attribution",
+                                            wagtail_blocks.CharBlock(required=False),
+                                        ),
+                                    ]
+                                ),
+                            ),
+                            (
+                                "pull_quote",
+                                wagtail_blocks.StructBlock(
+                                    [
+                                        (
+                                            "pull_quote",
+                                            wagtail_blocks.CharBlock(classname="title"),
+                                        )
+                                    ]
+                                ),
+                            ),
+                            (
+                                "embed",
+                                wagtail_blocks.StructBlock(
+                                    [
+                                        (
+                                            "title",
+                                            wagtail_blocks.CharBlock(required=False),
+                                        ),
+                                        ("embed", wagtail.embeds.blocks.EmbedBlock()),
+                                    ]
+                                ),
+                            ),
+                            (
+                                "video",
+                                wagtail_blocks.StructBlock(
+                                    [
+                                        (
+                                            "video",
+                                            wagtail.embeds.blocks.EmbedBlock(
+                                                label="Video URL", required=False
+                                            ),
+                                        )
+                                    ]
+                                ),
+                            ),
+                        ],
+                        blank=True,
+                    ),
+                ),
+                (
+                    "site",
+                    models.OneToOneField(
+                        editable=False,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="wagtailcore.Site",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Manage Editor Guide',
+                "verbose_name": "Manage Editor Guide",
             },
         ),
     ]
