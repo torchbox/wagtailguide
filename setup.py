6d4c7d114bce7575c88493a9fc8de8807ab3754b
Kevin <kbhowbrook@gmail.com>
2023-03-23 21:36:15 +0000

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="wagtail_guide",
-    version="1.0.8",
+    version="2.0.0",
     description="Adds functionality to add and edit a CMS guide page for editors.",
     long_description=long_description,
     long_description_content_type="text/markdown",
